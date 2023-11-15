19237c7194ccd1585c2bf2500b0d9f507e7d64cd
yaasir cheekoory <yaasir1997@gmail.com>
2023-11-15 22:11:29 +0400

    completed structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from 'react';
+import './index.css';
+import { Form } from 'react-bootstrap';
 
-function App() {
-  const [count, setCount] = useState(0)
+const UNSPLASH_API_URL = "https://api.unsplash.com/search/photos?page=1&query=office";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+const App = () => {
+  const searchInput = useRef(null);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    console.log(searchInput.current.value)
+  }
+
+  const handleSelection = (selection) => {
+    searchInput.current.value = selection;
+  }
+
+  return <div className='container'>
+    <h1 className='title'>Image Search</h1>
+    <div className="search-section">
+      <Form onSubmit={handleSearch}>
+        <Form.Control type="search" placeholder="Search images" className='search-input' ref={searchInput} />
+      </Form>
+    </div>
+
+    <div className="filters">
+      <div onClick={() => handleSelection("nature")}>Nature</div>
+      <div onClick={() => handleSelection("birds")}>Birds</div>
+      <div onClick={() => handleSelection("people")}>People</div>
+      <div onClick={() => handleSelection("sea")}>Sea</div>
+    </div>
+  </div>;
+};
+
+export default App;
