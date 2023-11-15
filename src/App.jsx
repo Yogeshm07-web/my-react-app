217ed884db23bab97c3ab0ebca12c92ee91312df
yaasir cheekoory <yaasir1997@gmail.com>
2023-11-15 23:54:01 +0400

    completed project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useRef, useState } from 'react';
-import './index.css';
+import { useEffect, useRef, useState } from 'react';
 import { Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-import { Button } from 'bootstrap';
+import './index.css';
 
 const UNSPLASH_API_URL = "https://api.unsplash.com/search/photos";
 const IMAGES_PER_PAGE = 5;
@@ -11,25 +11,31 @@ const IMAGES_PER_PAGE = 5;
 const App = () => {
   const searchInput = useRef(null);
   const [images, setImages] = useState([]);
-  const [page, setpage] = useState(2);
-
+  const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
 
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
+
+  const resetSearch = () => {
+    fetchImages();
+    setPage(1)
+  }
 
   const handleSearch = (event) => {
     event.preventDefault();
-    fetchImages();
+    resetSearch();
   }
 
   const handleSelection = (selection) => {
     searchInput.current.value = selection;
-    fetchImages();
+    resetSearch();
   }
 
   const fetchImages = async () => {
     try {
-      const { data } = await axios.get(`${UNSPLASH_API_URL}?query=${searchInput.current.value}&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_UNSPLASH_API_KEY}`);
-      console.log(data);
+      const { data } = await axios.get(`${UNSPLASH_API_URL}?query=${searchInput.current.value}&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_UNSPLASH_API_KEY}`);
       setImages(data.results);
       setTotalPages(data.total_pages);
 
@@ -39,7 +45,7 @@ const App = () => {
   }
 
   return <div className='container'>
-    <h1 className='title'>Image Search</h1>
+    <h1 className='title'>Unsplash Image Search</h1>
     <div className="search-section">
       <Form onSubmit={handleSearch}>
         <Form.Control type="search" placeholder="Search images" className='search-input' ref={searchInput} />
@@ -47,10 +53,10 @@ const App = () => {
     </div>
 
     <div className="filters">
-      <div onClick={() => handleSelection("nature")}>Nature</div>
-      <div onClick={() => handleSelection("birds")}>Birds</div>
+      <div onClick={() => handleSelection("travel")}>Travel</div>
+      <div onClick={() => handleSelection("animals")}>Animals</div>
       <div onClick={() => handleSelection("people")}>People</div>
-      <div onClick={() => handleSelection("sea")}>Sea</div>
+      <div onClick={() => handleSelection("patterns")}>Patterns</div>
     </div>
 
     <div className="images">
@@ -59,7 +65,7 @@ const App = () => {
           return (
             <img
               key={image.id}
-              src={image.urls.regular} 
+              src={image.urls.small} 
               alt={image.alt_description}
               className='image'
             />
@@ -69,10 +75,10 @@ const App = () => {
     </div>
 
 
-    {/* <div className="buttons">
-      {page > 1 && <Button>Previous</Button>}
-      {page < totalPages && <Button>Next</Button>}
-    </div> */}
+    <div className="buttons">
+      { page > 1 && <Button variant="warning" onClick={() => setPage(page - 1)}>Previous</Button> }
+      { page < totalPages && <Button variant="warning" onClick={() => setPage(page + 1)}>Next</Button> }
+    </div>
   </div>;
 };
 
